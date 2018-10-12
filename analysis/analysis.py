81e2413dccbf3afd6a525c81ea86a602816dcdc1
Peter Dekker <peter@peterdekker.eu>
2018-10-12 17:14:14 +0200

    Started analysis of answers

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -7,18 +7,40 @@ import errno
 import numpy as np
 from collections import defaultdict, Counter
 
-filenames = [('Blends analysis', 'results-sept2018/analyse_task_run.json'), ('Blends recognition', 'results-sept2018/herken1_task_run.json')]
+projects = [{'name':'Blends analysis',
+              'runs_filename': 'results-sept2018/analyse_task_run.json',
+              'gold_filename': 'results-sept2018/blends-analyse-10.csv',
+              'type': 'analyse'},
+             {'name': 'Blends recognition',
+              'runs_filename': 'results-sept2018/herken1_task_run.json',
+              'gold_filename': 'results-sept2018/blends-herken1-10.csv',
+              'type': 'herken1'}]
 details = ["gender","age","location"]
 
-def get_df(filename):
-    if not os.path.isfile(filename):
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
-    df_data = pd.read_json(filename,orient="records")
+def get_runs_df(json_filename):
+    if not os.path.isfile(json_filename):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), json_filename)
+    df_data = pd.read_json(json_filename,orient="records")
     return df_data
+
+def get_gold_dict(csv_filename, data_type):
+    df = pd.read_csv(csv_filename)
+    gold_dict = {}
+    for index,row in df.iterrows():
+        if data_type == 'analyse':
+            key=row['lemma']
+            comp1=row['basiswoord1']
+            comp2=row['basiswoord2']
+            value=[comp1,comp2]
+        elif data_type == 'herken1':
+            key=row['voorbeeld1']
+            word = row['lemma']
+            value = [word]
+        gold_dict[key] = value
+    return gold_dict
 
 def is_details(field):
     # Info field should contain all of: gender,age,location
-    
     return all(key in field for key in details)
 
 def is_number(s):
@@ -49,7 +71,6 @@ def fix_case(value):
 def categorical_df(data, dataType, y_label):
     if dataType == "Location":
         data_clean = [fix_case(value) for value in data]
-        print(data_clean)
         # Replace '' by not_given
         data_clean =  ["Not given" if value=='' else value for value in data_clean]
         category_items = list(Counter(data_clean).most_common(10))
@@ -61,9 +82,7 @@ def categorical_df(data, dataType, y_label):
         data_clean = [mapping[value] for value in data]
         category_dict = Counter(data_clean)
         category_items = list(category_dict.items())
-    print(category_items)
     category_df = pd.DataFrame.from_records(category_items, columns = [dataType,y_label])
-    print(category_df)
 
     return category_df
 
@@ -84,13 +103,20 @@ def analyze_details(info_fields, title):
             ax = sns.barplot(x=prop_c, y=y_label, data=cat_df)
         ax.set(xlabel=prop_c, ylabel=y_label)
         plt.title(title)
-        plt.show()
+        plt.savefig(title + " " + prop_c + ".png")
 
 def main():
-    for (proj,filename) in filenames:
-        df = get_df(filename)
+    for proj in projects:
+        # Retrieve runs from file
+        df = get_runs_df(proj["runs_filename"])
+        # Analyze personal details
         details_fields = df[df['info'].map(is_details)]
-        analyze_details(details_fields['info'], proj)
+        analyze_details(details_fields['info'], proj["name"])
+
+        # Analyze answers
+        gold_dict = get_gold_dict(proj["gold_filename"], data_type=proj["type"])
+        answer_fields = df[df['info'].map(not is_details)]
+        print(answer_fields["info"])
         
 
 if __name__ == "__main__":
